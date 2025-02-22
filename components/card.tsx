cae5838ba23d5a9c743ec47ebca43e24436e7533
sqlmerr <sqlmerr@ya.ru>
2025-02-22 22:49:20 +0300

    fix

--- components/card.tsx
+++ components/card.tsx
@@ -1,17 +1,13 @@
 "use client";
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useSpring,
-} from "framer-motion";
+import { motion, useMotionTemplate, useSpring } from "framer-motion";
 
-import { MouseEventHandler, PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 
 export const Card: React.FC<PropsWithChildren> = ({ children }) => {
   const mouseX = useSpring(0, { stiffness: 500, damping: 100 });
   const mouseY = useSpring(0, { stiffness: 500, damping: 100 });
 
+  // @typescript-eslint/no-explicit-any
   function onMouseMove({ currentTarget, clientX, clientY }: any) {
     const { left, top } = currentTarget.getBoundingClientRect();
     mouseX.set(clientX - left);
